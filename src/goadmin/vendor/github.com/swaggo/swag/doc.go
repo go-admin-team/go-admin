874fd34f7b9227213e3b566f25a8aa9ef6b70503
zhangwenjian <zwj777@live.com>
2020-03-16 11:19:21 +0800

    删除vendor

--- src/goadmin/vendor/github.com/swaggo/swag/doc.go
+++
@@ -1,5 +0,0 @@
-/*
-Package swag converts Go annotations to Swagger Documentation 2.0.
-See https://github.com/swaggo/swag for more information about swag.
-*/
-package swag // import "github.com/swaggo/swag"
--- src/goadmin/vendor/github.com/swaggo/swag/version.go
+++
@@ -1,4 +0,0 @@
-package swag
-
-// Version of swag
-const Version = "v1.5.1"
