3edd43e7a0b43f0526aac75c936e5dfc7f7a7172
zhangwenjian <zwj777@live.com>
2020-03-16 11:17:50 +0800

    演示环境过滤器

--- src/goadmin/router/middleware/demo.go
+++ src/goadmin/router/middleware/demo.go
@@ -10,7 +10,7 @@ func DemoEvn() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		if config.ApplicationConfig.Env == "demo" {
 			method := c.Request.Method
-			if method == "GET" || method == "OPTIONS" || c.Request.RequestURI == "/login" || c.Request.RequestURI == "/logout" {
+			if method == "GET" || method == "OPTIONS" || c.Request.RequestURI == "/login" || c.Request.RequestURI == "/api/v1/logout" {
 				c.Next()
 			} else {
 				c.JSON(http.StatusOK, gin.H{
