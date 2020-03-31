7af0f6d3fc2461b80d0edfe82a162744b3e5703c
wenjianzhang <zwj777@live.com>
2020-03-31 21:43:07 +0800

    Update dictData.go

--- models/dictData.go
+++ models/dictData.go
@@ -43,6 +43,7 @@ type DictData struct {
 func (e *DictData) Create() (DictData, error) {
 	var doc DictData
 	e.CreateTime = time.Now().String()
+	e.UpdateTime = utils.GetCurrntTime()
 	result := orm.Eloquent.Table("sys_dict_data").Create(&e)
 	if result.Error != nil {
 		err := result.Error
