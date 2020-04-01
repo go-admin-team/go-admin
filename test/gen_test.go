4bb5c883f655557467b40b83e89f99661b12a747
zhangwenjian <zwj777@live.com>
2020-04-01 14:39:47 +0800

    gen

---
+++ test/gen_test.go
@@ -0,0 +1,44 @@
+package test
+
+import (
+	"go-admin/models/tools"
+	"os"
+	"testing"
+	"text/template"
+)
+
+func TestGoModelTemplate(t *testing.T) {
+	t1, err := template.ParseFiles("model.go.template")
+	if err != nil {
+		t.Error(err)
+	}
+	table := tools.SysTables{}
+	table.TableName = "sys_tables"
+	tab, _ := table.Get()
+	file, err := os.Create("models/"+table.PackageName+".go")
+	if err != nil {
+		t.Error(err)
+	}
+	defer file.Close()
+
+	_ = t1.Execute(file, tab)
+	t.Log("")
+}
+
+func TestGoApiTemplate(t *testing.T) {
+	t1, err := template.ParseFiles("api.go.template")
+	if err != nil {
+		t.Error(err)
+	}
+	table := tools.SysTables{}
+	table.TableName = "sys_tables"
+	tab, _ := table.Get()
+	file, err := os.Create("apis/"+table.PackageName+".go")
+	if err != nil {
+		t.Error(err)
+	}
+	defer file.Close()
+
+	_ = t1.Execute(file, tab)
+	t.Log("")
+}
