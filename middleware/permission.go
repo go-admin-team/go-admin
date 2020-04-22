bf494c21310b9b8f10677e23c8e71d54adaf5eba
zhangwenjian <zwj777@live.com>
2020-04-22 23:16:20 +0800

    权限检查修改返回结构名称，修复前端无限循环getinfo接口bug

--- middleware/permission.go
+++ middleware/permission.go
@@ -16,19 +16,19 @@ func AuthCheckRole() gin.HandlerFunc {
 		data, _ := c.Get("JWT_PAYLOAD")
 		v := data.(jwtauth.MapClaims)
 		e, err := mycasbin.Casbin()
-		tools.HasError(err,"",500)
+		tools.HasError(err, "", 500)
 		//检查权限
 		res, err := e.Enforce(v["rolekey"], c.Request.URL.Path, c.Request.Method)
 		log.Println("----------------", v["rolekey"], c.Request.URL.Path, c.Request.Method)
 
-			tools.HasError(err,"",500)
+		tools.HasError(err, "", 500)
 
 		if res {
 			c.Next()
 		} else {
 			c.JSON(http.StatusOK, gin.H{
-				"status": 401,
-				"msg":    "对不起，您没有该接口访问权限，请联系管理员",
+				"code": 401,
+				"msg":  "对不起，您没有该接口访问权限，请联系管理员",
 			})
 			c.Abort()
 			return
