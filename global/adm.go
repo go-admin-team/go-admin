f95aba834564875ff503baa8ce608af00d1ae021
wenjianzhang <zwj777@live.com>
2020-07-22 17:22:20 +0800

    feat：Add version information

--- global/adm.go
+++ global/adm.go
@@ -11,6 +11,14 @@ var GinEngine *gin.Engine
 var Eloquent *gorm.DB
 var Source string
 var Driver string
+
 var DBName string
 
 var CasbinEnforcer *casbin.Enforcer
+
+// go-admin Version Info
+var Version string
+
+func init()  {
+	Version = "1.0.10"
+}
