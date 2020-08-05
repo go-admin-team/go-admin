18a2aeadaaaec522bdbc001a9e60c3f3d5bacb4e
linwenxiang <991154416@qq.com>
2020-08-05 23:22:09 +0800

    定义缓存和队列接口

---
+++ pkg/cache/type.go
@@ -0,0 +1,24 @@
+package cache
+
+import (
+	"time"
+)
+
+type Adapter interface {
+	Connect()
+	Get(key string) (string, error)
+	Set(key string, val interface{}, expire int) error
+	Del(key string) error
+	HashGet(hk, key string) (string, error)
+	HashDel(hk, key string) error
+	Increase(key string) error
+	Expire(key string, dur time.Duration) error
+	SetQueue(name string, message Message) error
+	GetQueue(name string, f func(message Message) error)
+}
+
+type Message struct {
+	ID     string
+	Stream string
+	Values map[string]interface{}
+}
