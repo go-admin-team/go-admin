7373536d3643351a3a11a73cc6feaf64e2b3b35e
linwenxiang <991154416@qq.com>
2020-08-25 23:10:09 +0800

    修改注释

--- pkg/table/analysis.go
+++ pkg/table/analysis.go
@@ -11,13 +11,13 @@ func Crc32Hash(src string) string {
 	return strconv.Itoa(int(sum))
 }
 
-// Crc16Hash 用于32张分表
+// Crc16Hash 用于16张分表
 func Crc16Hash(src string) string {
 	sum := crc32.ChecksumIEEE([]byte(src)) % 16
 	return strconv.Itoa(int(sum))
 }
 
-// Crc8Hash 用于32张分表
+// Crc8Hash 用于8张分表
 func Crc8Hash(src string) string {
 	sum := crc32.ChecksumIEEE([]byte(src)) % 8
 	return strconv.Itoa(int(sum))
