c851e82762f12ed7b748eaef85d8731ac7f4e980
zhangwenjian <zwj777@live.com>
2020-10-23 23:34:32 +0800

    docs:文档更新

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 // @description 基于Gin + Vue + Element UI的前后端分离权限管理系统的接口文档
 // @description 添加qq群: 74520518 进入技术交流群 请备注，谢谢！
 // @license.name MIT
-// @license.url https://github.com/wenjianzhang/go-admin/blob/master/LICENSE.md
+// @license.url https://github.com/go-admin-team/go-admin/blob/master/LICENSE.md
 
 // @securityDefinitions.apikey Bearer
 // @in header
