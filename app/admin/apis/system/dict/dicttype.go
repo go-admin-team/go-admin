ab547cc49bb64a41e363f49cfcc5aac8bfc5b13e
wenjianzhang <zwj777@live.com>
2020-11-29 00:26:17 +0800

    refactor:🎨 资源管理模块代码升级，移除旧代码

--- app/admin/apis/system/dict/dicttype.go
+++ app/admin/apis/system/dict/dicttype.go
@@ -19,7 +19,7 @@ import (
 // @Param dictType query string false "dictType"
 // @Param pageSize query int false "页条数"
 // @Param pageIndex query int false "页码"
-// @Success 200 {object} app.PageResponse "{"code": 200, "data": [...]}"
+// @Success 200 {object} app.Response "{"code": 200, "data": [...]}"
 // @Router /api/v1/dict/type/list [get]
 // @Security Bearer
 func GetDictTypeList(c *gin.Context) {
