e1f15bcff1f1ae8c12ee003d39ce724ac3ad1edf
zhangwenjian <zwj777@live.com>
2021-01-11 09:34:53 +0800

    添加返回log输出

--- tools/app/return.go
+++ tools/app/return.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"encoding/json"
+	"go-admin/common/log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,6 +12,7 @@ import (
 
 // 失败数据处理
 func Error(c *gin.Context, code int, err error, msg string) {
+	msgID := tools.GenerateMsgIDFromContext(c)
 	var res Response
 	if err != nil {
 		res.Msg = err.Error()
@@ -18,18 +21,33 @@ func Error(c *gin.Context, code int, err error, msg string) {
 		res.Msg = msg
 	}
 	res.RequestId = tools.GenerateMsgIDFromContext(c)
-	c.AbortWithStatusJSON(http.StatusOK, res.ReturnError(code))
+	Return := res.ReturnError(code)
+	var jsonStr []byte
+	jsonStr, err = json.Marshal(Return)
+	if err != nil {
+		log.Debugf("MsgID[%s] ShouldBind error: %#v", msgID, err.Error())
+	}
+	c.Set("result",string(jsonStr))
+	c.AbortWithStatusJSON(http.StatusOK, Return)
 }
 
 // 通常成功数据处理
 func OK(c *gin.Context, data interface{}, msg string) {
+	msgID := tools.GenerateMsgIDFromContext(c)
 	var res Response
 	res.Data = data
 	if msg != "" {
 		res.Msg = msg
 	}
 	res.RequestId = tools.GenerateMsgIDFromContext(c)
-	c.AbortWithStatusJSON(http.StatusOK, res.ReturnOK())
+	Return := res.ReturnOK()
+	var jsonStr []byte
+	jsonStr, err := json.Marshal(Return)
+	if err != nil {
+		log.Debugf("MsgID[%s] ShouldBind error: %#v", msgID, err.Error())
+	}
+	c.Set("result",string(jsonStr))
+	c.AbortWithStatusJSON(http.StatusOK, Return)
 }
 
 // 分页数据处理
@@ -44,5 +62,13 @@ func PageOK(c *gin.Context, result interface{}, count int, pageIndex int, pageSi
 
 // 兼容函数
 func Custum(c *gin.Context, data gin.H) {
-	c.AbortWithStatusJSON(http.StatusOK, data)
+	msgID := tools.GenerateMsgIDFromContext(c)
+	Return := data
+	var jsonStr []byte
+	jsonStr, err := json.Marshal(Return)
+	if err != nil {
+		log.Debugf("MsgID[%s] ShouldBind error: %#v", msgID, err.Error())
+	}
+	c.Set("result",string(jsonStr))
+	c.AbortWithStatusJSON(http.StatusOK, Return)
 }
