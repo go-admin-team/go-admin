ee15329bd24b5f812dc106247c29973c3ce38b58
wenjianzhang <zwj777@live.com>
2021-05-11 18:53:40 +0800

    feat✨ systables 数据迁移模块更新

--- cmd/migrate/migration/models/systables.go
+++ cmd/migrate/migration/models/systables.go
@@ -1,16 +1,17 @@
 package models
 
 type SysTables struct {
-	TableId             int    `gorm:"primaryKey;autoIncrement" json:"tableId"`      //表编码
-	TBName              string `gorm:"column:table_name;size:255;" json:"tableName"` //表名称
-	TableComment        string `gorm:"size:255;" json:"tableComment"`                //表备注
-	ClassName           string `gorm:"size:255;" json:"className"`                   //类名
-	TplCategory         string `gorm:"size:255;" json:"tplCategory"`                 //
-	PackageName         string `gorm:"size:255;" json:"packageName"`                 //包名
-	ModuleName          string `gorm:"size:255;" json:"moduleName"`                  //模块名
-	BusinessName        string `gorm:"size:255;" json:"businessName"`                //
-	FunctionName        string `gorm:"size:255;" json:"functionName"`                //功能名称
-	FunctionAuthor      string `gorm:"size:255;" json:"functionAuthor"`              //功能作者
+	TableId             int    `gorm:"primaryKey;autoIncrement" json:"tableId"`        //表编码
+	TBName              string `gorm:"column:table_name;size:255;" json:"tableName"`   //表名称
+	TableComment        string `gorm:"size:255;" json:"tableComment"`                  //表备注
+	ClassName           string `gorm:"size:255;" json:"className"`                     //类名
+	TplCategory         string `gorm:"size:255;" json:"tplCategory"`                   //
+	PackageName         string `gorm:"size:255;" json:"packageName"`                   //包名
+	ModuleName          string `gorm:"size:255;" json:"moduleName"`                    //go文件名
+	ModuleFrontName     string `gorm:"size:255;comment:前端文件名;" json:"moduleFrontName"` //前端文件名
+	BusinessName        string `gorm:"size:255;" json:"businessName"`                  //
+	FunctionName        string `gorm:"size:255;" json:"functionName"`                  //功能名称
+	FunctionAuthor      string `gorm:"size:255;" json:"functionAuthor"`                //功能作者
 	PkColumn            string `gorm:"size:255;" json:"pkColumn"`
 	PkGoField           string `gorm:"size:255;" json:"pkGoField"`
 	PkJsonField         string `gorm:"size:255;" json:"pkJsonField"`
@@ -18,8 +19,8 @@ type SysTables struct {
 	TreeCode            string `gorm:"size:255;" json:"treeCode"`
 	TreeParentCode      string `gorm:"size:255;" json:"treeParentCode"`
 	TreeName            string `gorm:"size:255;" json:"treeName"`
-	Tree                bool   `gorm:"size:1;" json:"tree"`
-	Crud                bool   `gorm:"size:1;" json:"crud"`
+	Tree                bool   `gorm:"size:1;default:0;" json:"tree"`
+	Crud                bool   `gorm:"size:1;default:1;" json:"crud"`
 	Remark              string `gorm:"size:255;" json:"remark"`
 	IsDataScope         int    `gorm:"size:1;" json:"isDataScope"`
 	IsActions           int    `gorm:"size:1;" json:"isActions"`
