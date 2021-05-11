76e83de8cd99b05f1c204e23bc15738a05b64ac4
wenjianzhang <zwj777@live.com>
2021-05-11 18:42:56 +0800

    feat✨ 重置dept router

--- app/admin/router/sys_dept.go
+++ app/admin/router/sys_dept.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	jwt "github.com/go-admin-team/go-admin-core/sdk/pkg/jwtauth"
 	"go-admin/app/admin/apis"
-	middleware2 "go-admin/common/middleware"
+	"go-admin/common/middleware"
 )
 
 func init() {
@@ -14,7 +14,8 @@ func init() {
 // 需认证的路由代码
 func registerSysDeptRouter(v1 *gin.RouterGroup, authMiddleware *jwt.GinJWTMiddleware) {
 	api := apis.SysDept{}
-	r := v1.Group("/dept").Use(authMiddleware.MiddlewareFunc()).Use(middleware2.AuthCheckRole())
+
+	r := v1.Group("/dept").Use(authMiddleware.MiddlewareFunc()).Use(middleware.AuthCheckRole())
 	{
 		r.GET("", api.GetSysDeptList)
 		r.GET("/:id", api.GetSysDept)
