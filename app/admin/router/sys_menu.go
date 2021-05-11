c505884cf24964a2883ecb860ee5bf9a07302039
wenjianzhang <zwj777@live.com>
2021-05-11 18:40:59 +0800

    feat✨ 修改删除接口传参方式

--- app/admin/router/sys_menu.go
+++ app/admin/router/sys_menu.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	jwt "github.com/go-admin-team/go-admin-core/sdk/pkg/jwtauth"
 	"go-admin/app/admin/apis"
-	middleware2 "go-admin/common/middleware"
+	"go-admin/common/middleware"
 )
 
 func init() {
@@ -14,20 +14,14 @@ func init() {
 // 需认证的路由代码
 func registerSysMenuRouter(v1 *gin.RouterGroup, authMiddleware *jwt.GinJWTMiddleware) {
 	api := apis.SysMenu{}
-	//menu := v1.Group("/menu").Use(authMiddleware.MiddlewareFunc()).Use(middleware.AuthCheckRole())
-	//{
-	//	menu.GET("/:id", system.GetMenu)
-	//	menu.POST("", system.InsertMenu)
-	//	menu.PUT("", system.UpdateMenu)
-	//	menu.DELETE("/:id", system.DeleteMenu)
-	//}
-	r := v1.Group("/menu").Use(authMiddleware.MiddlewareFunc()).Use(middleware2.AuthCheckRole())
+
+	r := v1.Group("/menu").Use(authMiddleware.MiddlewareFunc()).Use(middleware.AuthCheckRole())
 	{
 		r.GET("", api.GetSysMenuList)
 		r.GET("/:id", api.GetSysMenu)
 		r.POST("", api.InsertSysMenu)
 		r.PUT("/:id", api.UpdateSysMenu)
-		r.DELETE("/:id", api.DeleteSysMenu)
+		r.DELETE("", api.DeleteSysMenu)
 	}
 
 	r1 := v1.Group("").Use(authMiddleware.MiddlewareFunc())
