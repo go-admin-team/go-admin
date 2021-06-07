c96d9c35a70f0467183021e78e08e5bbd3c481af
wenjianzhang <zwj777@live.com>
2021-06-07 18:05:05 +0800

    refactor🎨 ：支持名称和编码模糊搜索

--- app/admin/service/dto/sys_post.go
+++ app/admin/service/dto/sys_post.go
@@ -10,12 +10,12 @@ import (
 // SysPostSearch 列表或者搜索使用结构体
 type SysPostSearch struct {
 	dto.Pagination `search:"-"`
-	PostId         int    `form:"postId" search:"type:exact;column:post_id;table:sys_post" comment:"id"`     // id
-	PostName       string `form:"postName" search:"type:exact;column:post_name;table:sys_post" comment:"名称"` // 名称
-	PostCode       string `form:"postCode" search:"type:exact;column:post_code;table:sys_post" comment:"编码"` // 编码
-	Sort           int    `form:"sort" search:"type:exact;column:sort;table:sys_post" comment:"排序"`          // 排序
-	Status         int    `form:"status" search:"type:exact;column:status;table:sys_post" comment:"状态"`      // 状态
-	Remark         string `form:"remark" search:"type:exact;column:remark;table:sys_post" comment:"备注"`      // 备注
+	PostId         int    `form:"postId" search:"type:exact;column:post_id;table:sys_post" comment:"id"`        // id
+	PostName       string `form:"postName" search:"type:contains;column:post_name;table:sys_post" comment:"名称"` // 名称
+	PostCode       string `form:"postCode" search:"type:contains;column:post_code;table:sys_post" comment:"编码"` // 编码
+	Sort           int    `form:"sort" search:"type:exact;column:sort;table:sys_post" comment:"排序"`             // 排序
+	Status         int    `form:"status" search:"type:exact;column:status;table:sys_post" comment:"状态"`         // 状态
+	Remark         string `form:"remark" search:"type:exact;column:remark;table:sys_post" comment:"备注"`         // 备注
 }
 
 func (m *SysPostSearch) GetNeedSearch() interface{} {
