3d620d0149c72f3c8cfcbefd32871cbe3c958c99
zhangwenjian <zwj777@live.com>
2021-06-10 11:22:53 +0800

    refactor🎨: 添加高德地图key自定义扩展配置

--- config/extend.go
+++ config/extend.go
@@ -8,9 +8,9 @@ var ExtConfig Extend
 //      name: demo-name
 // 使用方法： config.ExtConfig......即可！！
 type Extend struct {
-	// Demo Demo   // 这里配置对应配置文件的结构即可
+	AMap AMap   // 这里配置对应配置文件的结构即可
 }
 
-type Demo struct {
-	Name string
+type AMap struct {
+	Key string
 }
