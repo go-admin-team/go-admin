6d3ac74fdaf5d0b453e276c3dc374d594f91f888
ninstein <ninstein@gmail.com>
2021-06-21 14:53:30 +0800

    bugfix：去掉废弃表；修复字段顺序错误

--- config/db.sql
+++ config/db.sql
@@ -192,9 +192,6 @@ INSERT INTO `sys_dict_type` VALUES (10, '通知状态', 'sys_notice_status', '2'
 INSERT INTO `sys_dict_type` VALUES (11, '内容状态', 'sys_content_status', '2', '', 1, 1, '2021-05-13 19:56:40.813', '2021-05-13 19:56:40.813', NULL);
 COMMIT;
 
-BEGIN;
-INSERT INTO `sys_file_dir` VALUES (1, '根目录', 0, 0, '', 1, 1, '2021-05-13 19:56:39.773', '2021-05-13 19:56:39.773', NULL);
-COMMIT;
 
 BEGIN;
 INSERT INTO `sys_job` VALUES (1, '接口测试', 'DEFAULT', 1, '0/5 * * * * ', 'http://localhost:8000', '', 1, 1, 1, 0, '2021-05-13 19:56:37.914', '2021-06-14 20:59:55.417', NULL, 1, 1);
@@ -358,11 +355,8 @@ INSERT INTO `sys_menu_api_rule` VALUES (45, 142);
 INSERT INTO `sys_menu_api_rule` VALUES (43, 150);
 INSERT INTO `sys_menu_api_rule` VALUES (45, 151);
 INSERT INTO `sys_menu_api_rule` VALUES (46, 156);
-INSERT INTO `sys_post` VALUES (1, '首席执行官', 'CEO', 0, '2', '首席执行官', '2021-05-13 19:56:37.913', '2021-05-13 19:56:37.913', NULL, 1, 1);
-INSERT INTO `sys_post` VALUES (2, '首席技术执行官', 'CTO', 2, '2', '首席技术执行官', '2021-05-13 19:56:37.913', '2021-05-13 19:56:37.913', NULL, 1, 1);
-INSERT INTO `sys_post` VALUES (3, '首席运营官', 'COO', 3, '2', '测试工程师', '2021-05-13 19:56:37.913', '2021-05-13 19:56:37.913', NULL, 1, 1);
-INSERT INTO `sys_role` VALUES (1, '系统管理员', '2', 'admin', 1, '', '', 1, '', '2021-05-13 19:56:37.913', '2021-05-13 19:56:37.913', NULL, 1, 1);
-INSERT INTO `sys_user` VALUES (1, 1, '2021-05-13 19:56:37.914', '2021-05-13 19:56:40.205', NULL, 1, 'admin', '$2a$10$/Glr4g9Svr6O0kvjsRJCXu3f0W8/dsP3XZyVNi1019ratWpSPMyw.', 'zhangwj', '13818888888', 1, '', '', '1', '1@qq.com', 1, 1, '', '2');
+INSERT INTO `sys_role` VALUES (1, '系统管理员', '2', 'admin', 1, '', '', 1, '', 1, 1, '2021-05-13 19:56:37.913', '2021-05-13 19:56:37.913', NULL);
+INSERT INTO `sys_user` VALUES (1, 'admin', '$2a$10$/Glr4g9Svr6O0kvjsRJCXu3f0W8/dsP3XZyVNi1019ratWpSPMyw.', 'zhangwj', '13818888888', 1, '', '', '1', '1@qq.com', 1, 1, '', '2', 1, 1, '2021-05-13 19:56:37.914', '2021-05-13 19:56:40.205', NULL);
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
