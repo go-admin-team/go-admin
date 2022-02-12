f114424079b7e55ad5ad61af150a95acaf459cb4
zhangwenjian <zwj777@live.com>
2022-02-13 00:23:20 +0800

    fix🐛: fix monitor macos env error

--- app/other/apis/sys_server_monitor.go
+++ app/other/apis/sys_server_monitor.go
@@ -2,18 +2,21 @@ package apis
 
 import (
 	"fmt"
-	"github.com/shirou/gopsutil/host"
+	"github.com/shirou/gopsutil/v3/net"
+	"regexp"
 	"runtime"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-admin-team/go-admin-core/sdk/api"
 	"github.com/go-admin-team/go-admin-core/sdk/pkg"
 	_ "github.com/go-admin-team/go-admin-core/sdk/pkg/response"
-	"github.com/shirou/gopsutil/cpu"
-	"github.com/shirou/gopsutil/disk"
-	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/disk"
+	"github.com/shirou/gopsutil/v3/host"
+	"github.com/shirou/gopsutil/v3/mem"
 )
 
 const (
@@ -23,11 +26,28 @@ const (
 	GB = 1024 * MB
 )
 
+var (
+	Version           string
+	expectDiskFsTypes = []string{
+		"apfs", "ext4", "ext3", "ext2", "f2fs", "reiserfs", "jfs", "btrfs",
+		"fuseblk", "zfs", "simfs", "ntfs", "fat32", "exfat", "xfs", "fuse.rclone",
+	}
+	excludeNetInterfaces = []string{
+		"lo", "tun", "docker", "veth", "br-", "vmbr", "vnet", "kube",
+	}
+	getMacDiskNo = regexp.MustCompile(`\/dev\/disk(\d)s.*`)
+)
+
+var (
+	netInSpeed, netOutSpeed, netInTransfer, netOutTransfer, lastUpdateNetStats uint64
+	cachedBootTime                                                             time.Time
+)
+
 type ServerMonitor struct {
 	api.Api
 }
 
-//获取相差时间
+// GetHourDiffer 获取相差时间
 func GetHourDiffer(startTime, endTime string) int64 {
 	var hour int64
 	t1, err := time.ParseInLocation("2006-01-02 15:04:05", startTime, time.Local)
@@ -64,33 +84,28 @@ func (e ServerMonitor) ServerInfo(c *gin.Context) {
 	osDic["hostName"] = sysInfo.Hostname
 	osDic["time"] = time.Now().Format("2006-01-02 15:04:05")
 
-	dis, _ := disk.Usage("/")
-	diskTotalGB := int(dis.Total) / GB
-	diskFreeGB := int(dis.Free) / GB
-	diskDic := make(map[string]interface{}, 0)
-	diskDic["total"] = diskTotalGB
-	diskDic["free"] = diskFreeGB
-
 	mem, _ := mem.VirtualMemory()
-	memUsedMB := int(mem.Used) / GB
-	memTotalMB := int(mem.Total) / GB
-	memFreeMB := int(mem.Free) / GB
-	memUsedPercent := int(mem.UsedPercent)
 	memDic := make(map[string]interface{}, 0)
-	memDic["total"] = memTotalMB
-	memDic["used"] = memUsedMB
-	memDic["free"] = memFreeMB
-	memDic["usage"] = memUsedPercent
+	memDic["used"] = mem.Used / MB
+	memDic["total"] = mem.Total / MB
+
+	fmt.Println("mem", int(mem.Total/mem.Used*100))
+	memDic["percent"] = pkg.Round(mem.UsedPercent, 2)
+
+	swapDic := make(map[string]interface{}, 0)
+	swapDic["used"] = mem.SwapTotal - mem.SwapFree
+	swapDic["total"] = mem.SwapTotal
 
 	cpuDic := make(map[string]interface{}, 0)
 	cpuDic["cpuInfo"], _ = cpu.Info()
 	percent, _ := cpu.Percent(0, false)
-	cpuDic["Percent"] = pkg.Round(percent[0], 2)
+	cpuDic["percent"] = pkg.Round(percent[0], 2)
 	cpuDic["cpuNum"], _ = cpu.Counts(false)
 
 	//服务器磁盘信息
 	disklist := make([]disk.UsageStat, 0)
 	//所有分区
+	var diskTotal, diskUsed, diskUsedPercent float64
 	diskInfo, err := disk.Partitions(true)
 	if err == nil {
 		for _, p := range diskInfo {
@@ -101,16 +116,72 @@ func (e ServerMonitor) ServerInfo(c *gin.Context) {
 				diskDetail.Used = diskDetail.Used / 1024 / 1024
 				diskDetail.Free = diskDetail.Free / 1024 / 1024
 				disklist = append(disklist, *diskDetail)
+
 			}
 		}
 	}
 
+	d, _ := disk.Usage("/")
+
+	diskTotal = float64(d.Total / GB)
+	diskUsed = float64(d.Used / GB)
+	diskUsedPercent, _ = strconv.ParseFloat(fmt.Sprintf("%.2f", d.UsedPercent), 64)
+
+	diskDic := make(map[string]interface{}, 0)
+	diskDic["total"] = diskTotal
+	diskDic["used"] = diskUsed
+	diskDic["percent"] = diskUsedPercent
+
+	bootTime, _ := host.BootTime()
+	cachedBootTime = time.Unix(int64(bootTime), 0)
+
+	TrackNetworkSpeed()
+	netDic := make(map[string]interface{}, 0)
+	netDic["in"] = pkg.Round(float64(netInSpeed/KB), 2)
+	netDic["out"] = pkg.Round(float64(netOutSpeed/KB), 2)
 	e.Custom(gin.H{
-		"code": 200,
-		"os":   osDic,
-		"mem":  memDic,
-		"cpu":  cpuDic,
-		"disk": diskDic,
-		"diskList": disklist,
+		"code":     200,
+		"os":       osDic,
+		"mem":      memDic,
+		"cpu":      cpuDic,
+		"disk":     diskDic,
+		"net":      netDic,
+		"swap":     swapDic,
+		"location": "Aliyun",
+		"bootTime": GetHourDiffer(cachedBootTime.Format("2006-01-02 15:04:05"), time.Now().Format("2006-01-02 15:04:05")),
 	})
+}
+
+func TrackNetworkSpeed() {
+	var innerNetInTransfer, innerNetOutTransfer uint64
+	nc, err := net.IOCounters(true)
+	if err == nil {
+		for _, v := range nc {
+			if isListContainsStr(excludeNetInterfaces, v.Name) {
+				continue
+			}
+			innerNetInTransfer += v.BytesRecv
+			innerNetOutTransfer += v.BytesSent
+		}
+		now := uint64(time.Now().Unix())
+		diff := now - lastUpdateNetStats
+		if diff > 0 {
+			netInSpeed = (innerNetInTransfer - netInTransfer) / diff
+			fmt.Println("netInSpeed", netInSpeed)
+			netOutSpeed = (innerNetOutTransfer - netOutTransfer) / diff
+			fmt.Println("netOutSpeed", netOutSpeed)
+		}
+		netInTransfer = innerNetInTransfer
+		netOutTransfer = innerNetOutTransfer
+		lastUpdateNetStats = now
+	}
+}
+
+func isListContainsStr(list []string, str string) bool {
+	for i := 0; i < len(list); i++ {
+		if strings.Contains(str, list[i]) {
+			return true
+		}
+	}
+	return false
 }
