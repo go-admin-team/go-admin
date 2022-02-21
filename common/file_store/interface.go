cdb5faf043c64cf71e45bada2494daab10f4830b
zhangwenjian <zwj777@live.com>
2022-02-21 18:06:11 +0800

    refactor🎨:  upgrade OXS interface

--- common/file_store/interface.go
+++ common/file_store/interface.go
@@ -1,6 +1,27 @@
 package file_store
 
+// DriverType 驱动类型
+type DriverType string
+
+const (
+	// HuaweiOBS 华为云OBS
+	HuaweiOBS DriverType = "HuaweiOBS"
+	// AliYunOSS 阿里云OSS
+	AliYunOSS DriverType = "AliYunOSS"
+	// QiNiuKodo 七牛云kodo
+	QiNiuKodo DriverType = "QiNiuKodo"
+)
+
+type ClientOption map[string]interface{}
+
+// TODO: FileStoreType名称待定
+
+// FileStoreType OXS
 type FileStoreType interface {
-	Setup() error
-	UpLoad(yourObjectName string, localFile string) error
+	// Setup 装载 endpoint sss
+	Setup(endpoint, accessKeyID, accessKeySecret, BucketName string, options ...ClientOption) error
+	// UpLoad 上传
+	UpLoad(yourObjectName string, localFile interface{}) error
+	// GetTempToken 获取临时Token
+	GetTempToken() (string, error)
 }
