8754ff81471ccd9a32ac2b26e8c360598d953b35
zhangwenjian <zwj777@live.com>
2022-02-21 18:04:18 +0800

    refactor🎨:  upgrade oss

--- common/file_store/oss.go
+++ common/file_store/oss.go
@@ -6,50 +6,43 @@ import (
 )
 
 type ALiYunOSS struct {
-	Client          interface{}
-	Endpoint        string
-	AccessKeyId     string
-	AccessKeySecret string
-	BucketName      string
+	Client     interface{}
+	BucketName string
 }
 
-func (e *ALiYunOSS) Setup() error {
-	// TODO: 如果需要使用阿里云OSS请在此处填写对应信息
-	e.AccessKeyId = ""
-	e.AccessKeySecret = ""
-	e.Endpoint = ""
-	e.BucketName = ""
-
-	client, err := oss.New(e.Endpoint, e.AccessKeyId, e.AccessKeySecret)
+//Setup 装载
+//endpoint sss
+func (e *ALiYunOSS) Setup(endpoint, accessKeyID, accessKeySecret, BucketName string, options ...ClientOption) error {
+	client, err := oss.New(endpoint, accessKeyID, accessKeySecret)
 	if err != nil {
 		log.Println("Error:", err)
 		return err
 	}
 	e.Client = client
+	e.BucketName = BucketName
+
 	return nil
 }
 
 // UpLoad 文件上传
-func (e *ALiYunOSS) UpLoad(yourObjectName string, localFile string) error {
-	err := e.Setup()
-	if err != nil {
-		log.Println("Error:", err)
-		return err
-	}
+func (e *ALiYunOSS) UpLoad(yourObjectName string, localFile interface{}) error {
 	// 获取存储空间。
 	bucket, err := e.Client.(*oss.Client).Bucket(e.BucketName)
 	if err != nil {
 		log.Println("Error:", err)
 		return err
 	}
-
 	// 设置分片大小为100 KB，指定分片上传并发数为3，并开启断点续传上传。
 	// 其中<yourObjectName>与objectKey是同一概念，表示断点续传上传文件到OSS时需要指定包含文件后缀在内的完整路径，例如abc/efg/123.jpg。
 	// "LocalFile"为filePath，100*1024为partSize。
-	err = bucket.UploadFile(yourObjectName, localFile, 100*1024, oss.Routines(3), oss.Checkpoint(true, ""))
+	err = bucket.UploadFile(yourObjectName, localFile.(string), 100*1024, oss.Routines(3), oss.Checkpoint(true, ""))
 	if err != nil {
 		log.Println("Error:", err)
 		return err
 	}
 	return nil
+}
+
+func (e *ALiYunOSS) GetTempToken() (string, error) {
+	return "", nil
 }
