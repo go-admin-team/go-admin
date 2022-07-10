3521e389599f7e70ae190c300f2fbfca88955fa6
Kane <kaneleven@users.noreply.github.com>
2022-07-10 14:51:47 +0800

    Revert "feat✨:  added obs,kodo"
    This reverts commit ae5b668ef95fa8800b4a8db17aa594b52c96e48d.

--- common/file_store/initialize.go
+++ common/file_store/initialize.go
@@ -1,45 +1,13 @@
 package file_store
 
-import "fmt"
-
-type OXS struct {
-	// Endpoint 访问域名
-	Endpoint string
-	// AccessKeyID AK
-	AccessKeyID string
-	// AccessKeySecret AKS
-	AccessKeySecret string
-	// BucketName 桶名称
-	BucketName string
+type FileStore struct {
 }
 
-// Setup 配置文件存储driver
-func (e *OXS) Setup(driver DriverType, options ...ClientOption) FileStoreType {
-	fileStoreType := driver
-	var fileStore FileStoreType
-	switch fileStoreType {
-	case AliYunOSS:
-		fileStore = new(ALiYunOSS)
-		err := fileStore.Setup(e.Endpoint, e.AccessKeyID, e.AccessKeySecret, e.BucketName)
-		if err != nil {
-			fmt.Println(err)
-		}
-		return fileStore
-	case HuaweiOBS:
-		fileStore = new(HuaWeiOBS)
-		err := fileStore.Setup(e.Endpoint, e.AccessKeyID, e.AccessKeySecret, e.BucketName)
-		if err != nil {
-			fmt.Println(err)
-		}
-		return fileStore
-	case QiNiuKodo:
-		fileStore = new(QiNiuKODO)
-		err := fileStore.Setup(e.Endpoint, e.AccessKeyID, e.AccessKeySecret, e.BucketName)
-		if err != nil {
-			fmt.Println(err)
-		}
-		return fileStore
-	}
-
-	return nil
+// Setup 配置数据库
+func (e *FileStore) Setup(driver string) {
+	//fileStoreType := driver
+	//if fileStoreType == "ALiYunOSS" {
+	//	var store = new(ALiYunOSS)
+	//	store.Setup()
+	//}
 }
