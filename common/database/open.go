8fdd24ad50da9f3d3f3982ba5b2e98aee8e95230
xiaobo <xiaobo@uniontech.com>
2022-10-27 15:53:50 +0800

    fix: fix panic when use sqlite3

--- common/database/open.go
+++ common/database/open.go
@@ -6,6 +6,7 @@ package database
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
 	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
 )
@@ -13,5 +14,6 @@ import (
 var opens = map[string]func(string) gorm.Dialector{
 	"mysql":     mysql.Open,
 	"postgres":  postgres.Open,
+	"sqlite3":   sqlite.Open,
 	"sqlserver": sqlserver.Open,
 }
