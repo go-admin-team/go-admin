fa73c3d6b103457d6c18f9a585d620bac65a2ff1
wenjianzhang <zwj777@live.com>
2022-11-03 14:55:02 +0800

    patch🚑:  update restart

--- restart.sh
+++ restart.sh
@@ -2,6 +2,7 @@
 echo "go build"
 go mod tidy
 go build -o go-admin main.go
+chmod +x ./go-admin
 echo "kill go-admin service"
 killall go-admin # kill go-admin service
 nohup ./go-admin server -c=config/settings.dev.yml >> access.log 2>&1 & #后台启动服务将日志写入access.log文件
