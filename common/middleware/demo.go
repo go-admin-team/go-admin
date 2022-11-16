7d7d8484d4bfc0e560d909d964acca77e19b8ffe
wenjianzhang <zwj777@live.com>
2022-11-16 12:05:33 +0800

    fix🐛:  demo中间件添加环境判断

--- common/middleware/demo.go
+++ common/middleware/demo.go
@@ -8,19 +8,21 @@ import (
 func DemoEvn() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		method := c.Request.Method
-		if method == "GET" ||
-			method == "OPTIONS" ||
-			c.Request.RequestURI == "/api/v1/login" ||
-			c.Request.RequestURI == "/api/v1/logout" {
-			c.Next()
-		} else {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 500,
-				"msg":  "谢谢您的参与，但为了大家更好的体验，所以本次提交就算了吧！\U0001F600\U0001F600\U0001F600",
-			})
-			c.Abort()
-			return
+		if config.ApplicationConfig.Mode == "demo" {
+			if method == "GET" ||
+				method == "OPTIONS" ||
+				c.Request.RequestURI == "/api/v1/login" ||
+				c.Request.RequestURI == "/api/v1/logout" {
+				c.Next()
+			} else {
+				c.JSON(http.StatusOK, gin.H{
+					"code": 500,
+					"msg":  "谢谢您的参与，但为了大家更好的体验，所以本次提交就算了吧！\U0001F600\U0001F600\U0001F600",
+				})
+				c.Abort()
+				return
+			}
 		}
-
+		c.Next()
 	}
 }
