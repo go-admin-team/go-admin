a1a5634c4ed4e353c870f6e447306ec616aa3c2d
Akiraka <akiraka@outlook.com>
2023-04-14 19:51:22 +0800

    恢复修改

--- common/ip.go
+++ common/ip.go
@@ -8,7 +8,7 @@ import (
 func GetClientIP(c *gin.Context) string {
 	ClientIP := c.ClientIP()
 	//fmt.Println("ClientIP:", ClientIP)
-	RemoteIP := c.RemoteIP()
+	RemoteIP, _ := c.RemoteIP()
 	//fmt.Println("RemoteIP:", RemoteIP)
 	ip := c.Request.Header.Get("X-Forwarded-For")
 	if strings.Contains(ip, "127.0.0.1") || ip == "" {
@@ -17,8 +17,8 @@ func GetClientIP(c *gin.Context) string {
 	if ip == "" {
 		ip = "127.0.0.1"
 	}
-	if RemoteIP != "127.0.0.1" {
-		ip = RemoteIP
+	if RemoteIP.String() != "127.0.0.1" {
+		ip = RemoteIP.String()
 	}
 	if ClientIP != "127.0.0.1" {
 		ip = ClientIP
